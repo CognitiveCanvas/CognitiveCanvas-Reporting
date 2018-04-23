a0473dc1a4e89fb5f8c1f8926a02081e810d1e28
Bhargav-Rao <bhargav.rao448@gmail.com>
2018-04-23 14:58:17 -0700

    Added an API to get actions per user

--- returnData.py
+++ returnData.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, jsonify, g, redirect, url_for
 from time import gmtime, strftime
 from getDataFromMongo import DbData
+from getDataFromSite import *
+
 
 app = Flask(__name__)
 mongo = DbData(app)
@@ -19,7 +21,14 @@ def display_posts():
 @app.route("/user/<int:user_id>")
 def get_user_data_by_user_id(user_id):
     list_of_users = mongo.get_user_by_id("id", user_id)
-    return jsonify({"status": "success", "users": list_of_users, "count": len(list_of_users)})
+    return jsonify({"status": "success", "users": list_of_users})
+
+
+@app.route("/user/<int:user_id>/actions")
+def get_user_action_data_by_user_id(user_id):
+    action_filter = request.args.get("filter", None) or request.form.get("filter",None)
+    list_of_users = get_action_data(user_id, action_filter)
+    return jsonify({"status": "success", "users": list_of_users})
 
 
 @app.route("/users/create")
