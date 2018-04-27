65e8beafc43c73151322ada171c807af50a4c2cc
Bhargav-Rao <bhargav.rao448@gmail.com>
2018-04-27 16:38:01 -0700

    Added a setup.py file for OpenShift

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='ccreporting',
+      version='1.0',
+      description='Cognitive Canvas Reporting',
+      author='Bhargav Rao',
+      author_email='bhargav.rao448@gmail.com',
+      url='https://www.python.org/community/sigs/current/distutils-sig',
+      install_requires=['Flask>=0.12.2'],
+      )
