230e155814f723109f357391264d208552425cd7
Bhargav-Rao <bhargav.rao448@gmail.com>
2018-04-28 23:29:32 -0700

    Added 2 new endpoints to check if user is online and to get users by type

--- returnData.py
+++ returnData.py
@@ -6,7 +6,7 @@ from getDataFromSite import *
 
 app = Flask(__name__)
 mongo = DummyData()
-#mongo = DbData(app)
+mongo = DbData(app)
 
 
 @app.errorhandler(404)
@@ -29,6 +29,18 @@ def get_user_data():
 @app.route("/users/<string:user_email>")
 def get_user_data_by_user_email(user_email):
     list_of_users = mongo.get_user_by_key_value("email", user_email)
+    return jsonify({"status": "success", "users": list_of_users})
+
+
+@app.route("/users/type/<string:user_type>")
+def get_user_data_by_user_type(user_type):
+    list_of_users = mongo.get_user_by_key_value("Type", user_type)
+    return jsonify({"status": "success", "users": list_of_users})
+
+
+@app.route("/users/online")
+def get_user_data_if_logged_in():
+    list_of_users = mongo.get_user_by_key_value("isLogin", True)
     return jsonify({"status": "success", "users": list_of_users})
 
 
