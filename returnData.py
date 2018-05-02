1e052363a18edc93cda48f062e940fa77b2b1045
Bhargav-Rao <bhargav.rao448@gmail.com>
2018-05-02 13:34:04 -0700

    Refactored to use headers while returning data

--- returnData.py
+++ returnData.py
@@ -6,70 +6,71 @@ from getDataFromSite import *
 
 app = Flask(__name__)
 mongo = DummyData()
+
+
 # mongo = DbData(app)
 
 
 @app.errorhandler(404)
 def page_not_found(error):
     print(error)
-    return jsonify({"status": "failure", "message": "route does not exist"}), 404
+    return return_json_data("failure", "message", "route does not exist"), 404
 
 
 @app.route("/")
 def display_posts():
-    return jsonify({"status": "success", "message": "system running since " + strftime("%Y-%m-%d %H:%M:%S", gmtime())})
+    return return_json_data("success", "message", "system running since " + strftime("%Y-%m-%d %H:%M:%S", gmtime()))
 
 
 @app.route("/users")
 def get_user_data():
     list_of_users = mongo.get_users()
-    return jsonify({"status": "success", "users": list_of_users})
+    return return_json_data("success", "users", list_of_users)
 
 
 @app.route("/users/<string:user_email>")
 def get_user_data_by_user_email(user_email):
     list_of_users = mongo.get_user_by_key_value("email", user_email)
-    return jsonify({"status": "success", "users": list_of_users})
+    return return_json_data("success", "users", list_of_users)
 
 
 @app.route("/users/type/<string:user_type>")
 def get_user_data_by_user_type(user_type):
     list_of_users = mongo.get_user_by_key_value("Type", user_type)
-    return jsonify({"status": "success", "users": list_of_users})
+    return return_json_data("success", "users", list_of_users)
 
 
 @app.route("/users/online")
 def get_user_data_if_logged_in():
     list_of_users = mongo.get_user_by_key_value("isLogin", True)
-    return jsonify({"status": "success", "users": list_of_users})
+    return return_json_data("success", "users", list_of_users)
 
 
 @app.route("/users/<string:user_email>/maps/created")
 def get_maps_created_by_user_email(user_email):
     list_of_users = mongo.get_user_by_key_value("email", user_email, fields=["MapsCreated"])
     if len(list_of_users) == 0:
-        return jsonify({"status": "success", "maps": []})
+        return return_json_data("success", "maps", [])
     list_of_maps = list_of_users[0]["MapsCreated"]
-    return jsonify({"status": "success", "maps": list_of_maps})
+    return return_json_data("success", "maps", list_of_maps)
 
 
 @app.route("/users/<string:user_email>/maps/accessed")
 def get_maps_accessed_by_user_email(user_email):
     list_of_users = mongo.get_user_by_key_value("email", user_email, fields=["MapsAccessed"])
     if len(list_of_users) == 0:
-        return jsonify({"status": "success", "maps": []})
+        return return_json_data({"status", "maps", []})
     list_of_maps = list_of_users[0]["MapsAccessed"]
-    return jsonify({"status": "success", "maps": list_of_maps})
+    return return_json_data("success", "maps", list_of_maps)
 
 
 @app.route("/users/create")
 def create_user():
     mongo.add_user({"name": request.args['name'], "id": request.args["id"]})
-    return jsonify({"status": "success", "message": "user added successfully"})
+    return return_json_data("success", "message", "user added successfully")
 
 
-def return_json_data(payload):
-    response = jsonify({"status": "success", "users": payload})
+def return_json_data(status, dtype, payload):
+    response = jsonify({"status": status, dtype: payload})
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
-
