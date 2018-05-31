3dceed3500e9102426a2dee8c2ba84d8ff056e14
Bhargav-Rao <bhargav.rao448@gmail.com>
2018-05-31 13:02:29 -0700

    added html5lib to the requirements

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='ccreporting',
       author='Bhargav Rao',
       author_email='bhargav.rao448@gmail.com',
       url='https://www.python.org/community/sigs/current/distutils-sig',
-      install_requires=['Flask>=0.12.2', 'Flask-PyMongo>=0.5', 'requests', 'selenium', 'beautifulsoup4'],
+      install_requires=['Flask>=0.12.2', 'Flask-PyMongo>=0.5', 'requests', 'selenium', 'beautifulsoup4', 'html5lib'],
       )
 
 system("curl -SL https://github.com/adieuadieu/serverless-chrome/releases/download/v1.0.0-46/stable-headless-chromium"
