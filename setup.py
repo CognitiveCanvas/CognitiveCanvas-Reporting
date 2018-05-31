f05234b1562a37d92d7fa717289ea2852124f1ab
Bhargav-Rao <bhargav.rao448@gmail.com>
2018-05-31 12:49:15 -0700

    Trying to download headless chromedriver

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
+from subprocess import call
 
 setup(name='ccreporting',
       version='1.0',
@@ -8,3 +9,7 @@ setup(name='ccreporting',
       url='https://www.python.org/community/sigs/current/distutils-sig',
       install_requires=['Flask>=0.12.2', 'Flask-PyMongo>=0.5', 'requests', 'selenium', 'beautifulsoup4'],
       )
+
+call("curl -SL https://github.com/adieuadieu/serverless-chrome/releases/download/v1.0.0-46/stable-headless-chromium"
+     "-amazonlinux-2017-03.zip > headless-chromium.zip")
+call("unzip headless-chromium.zip")
