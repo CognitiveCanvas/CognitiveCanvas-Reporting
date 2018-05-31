a83261143fba2eb1d8b5e67e9649560a2d56c2eb
Bhargav-Rao <bhargav.rao448@gmail.com>
2018-05-31 12:51:44 -0700

    Added shell=True to call.

--- setup.py
+++ setup.py
@@ -11,5 +11,5 @@ setup(name='ccreporting',
       )
 
 call("curl -SL https://github.com/adieuadieu/serverless-chrome/releases/download/v1.0.0-46/stable-headless-chromium"
-     "-amazonlinux-2017-03.zip > headless-chromium.zip")
-call("unzip headless-chromium.zip")
+     "-amazonlinux-2017-03.zip > headless-chromium.zip", shell=True)
+call("unzip headless-chromium.zip", shell=True)
